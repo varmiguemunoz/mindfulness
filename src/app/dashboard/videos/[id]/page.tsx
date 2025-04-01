2e564fe59b788cae574be1fc9a300b45df310c3b
varmiguemunoz <varmiguemunoz@gmail.com>
2025-03-31 23:11:43 -0500

    fix: resolve reproductor video issue

--- src/app/dashboard/videos/[id]/page.tsx
+++ src/app/dashboard/videos/[id]/page.tsx
@@ -4,14 +4,17 @@ import React, { useEffect, useState } from "react";
 import { getSuscriptionStatus } from "@/utils/api";
 import { redirect, usePathname } from "next/navigation";
 import { getVideo } from "@/utils/sanity";
+import Link from "next/link";
+import { toast } from "sonner";
+
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 export default function Videos() {
   const [videos, setVideos] = useState([]) as any;
 
   const pathname = usePathname();
 
-  const match = pathname?.match(/\/dashboard\/videos\/(\d+)/);
-  const id = match ? match[1] : (null as any);
+  const slug = pathname?.split("/").pop() as any;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,40 +26,48 @@ export default function Videos() {
         redirect("/dashboard");
       } else {
         try {
-          const data = await getVideo(id);
-          console.log(data);
+          const data = await getVideo(slug);
           setVideos(data);
         } catch (err) {
-          console.log(err);
+          toast.error(
+            "An error occurred while fetching the video. Please try again later."
+          );
+          throw err;
         }
       }
     };
 
     fetchData();
-  }, [id]);
+  }, [slug]);
 
   return (
     <div className="w-full mt-[118px] mx-auto pb-[100px]">
       {/* Reproductor de video */}
-      {videos?.video && (
-        <video
-          ref={videos.video}
-          className="w-full h-[90vh] object-cover"
-          controls
-          preload="metadata"
-          autoPlay
-          playsInline
-          crossOrigin="anonymous"
-          aria-label="Hentai Video Player"
-        ></video>
-      )}
+
+      <video
+        src={videos?.video}
+        className="w-full h-[90vh] object-cover"
+        controls
+        preload="metadata"
+        autoPlay
+        playsInline
+        crossOrigin="anonymous"
+        aria-label="Mindfulness Video Player"
+      ></video>
 
       {/* Información de la serie */}
       <section className="w-full flex justify-between items-top gap-20 py-16 max-w-[1214px] mx-auto max-md:flex-col max-md:px-4">
         <div className="md:w-[70%] flex flex-col gap-5">
-          <h1 className="text-lightPurple font-bold text-2xl">hoka</h1>
+          <div className="flex items-center gap-4">
+            <Link href={"/dashboard"}>
+              <FaArrowAltCircleLeft size={28} />
+            </Link>
+            <h1 className="text-lavender font-bold text-2xl">{videos.title}</h1>
+          </div>
 
-          <p className="text-lightGray text-md font-normal">descripcion</p>
+          <p className="text-black/50 text-md font-normal">
+            {videos.description}
+          </p>
         </div>
       </section>
     </div>
